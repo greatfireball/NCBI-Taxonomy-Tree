841073c1f4a8ffb1433091eb1461e8002f24b96e
Frank Förster <foersterfrank@gmx.de>
2015-05-29 08:37:49 +0200

    Inital commit with a module skeleton based on h2xs call

---
+++ lib/NCBI/Taxonomy/Tree.pm
@@ -0,0 +1,88 @@
+package NCBI::Taxonomy::Tree;
+
+use 5.010000;
+use strict;
+use warnings;
+
+our @ISA = qw();
+
+our $VERSION = '0.01';
+
+
+# Preloaded methods go here.
+
+1;
+__END__
+# Below is stub documentation for your module. You'd better edit it!
+
+=head1 NAME
+
+NCBI::Taxonomy::Tree - Perl extension for blah blah blah
+
+=head1 SYNOPSIS
+
+  use NCBI::Taxonomy::Tree;
+  blah blah blah
+
+=head1 DESCRIPTION
+
+Stub documentation for NCBI::Taxonomy::Tree, created by h2xs. It looks like the
+author of the extension was negligent enough to leave the stub
+unedited.
+
+Blah blah blah.
+
+=head1 HISTORY
+
+=over 8
+
+=item 0.01
+
+Original version; created by h2xs 1.23 with options
+
+  --omit-autoload
+	--omit-changes
+	--omit-XS
+	--gen-accessors
+	--compat-version
+	5.10.0
+	--omit-constant
+	--omit-enums
+	--omit-const-func
+	--name
+	NCBI::Taxonomy::Tree
+	--use-new-tests
+	--skip-exporter
+	--skip--autoloader
+	--version
+	0.01
+
+=back
+
+
+
+=head1 SEE ALSO
+
+Mention other useful documentation such as the documentation of
+related modules or operating system documentation (such as man pages
+in UNIX), or any relevant external documentation such as RFCs or
+standards.
+
+If you have a mailing list set up for your module, mention it here.
+
+If you have a web site set up for your module, mention it here.
+
+=head1 AUTHOR
+
+Frank Förster, E<lt>frank@E<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2015 by Frank Förster
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.18.2 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
